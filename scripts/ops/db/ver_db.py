254e4a322b583bffb796c1de6214a24c25bbf44d
Andres <cortesm_andres@javeriana.edu.co>
2026-05-27 16:57:14 -0500

    linux

--- scripts/ops/db/ver_db.py
+++ scripts/ops/db/ver_db.py
@@ -16,7 +16,7 @@ import sys
 from pathlib import Path
 from datetime import datetime
 
-ROOT = Path(__file__).resolve().parents[4]
+ROOT = Path(__file__).resolve().parents[3]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
